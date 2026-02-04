c1f5bbaec1cc0cda675320102777bcf066321e9e
jurarak151041 <juralak151041@gmail.com>
2026-02-04 17:26:05 +0700

    a

--- h/products.php
+++ h/products.php
@@ -1,21 +1,19 @@
+<?php
+	include_once("checklogin.php");
+?>
 <!doctype html>
 <html>
 <head>
 <meta charset="utf-8">
-<title>จัดการสินค้า - จุฬาลักษณ์</title>
+<title>หน้าจัดการสินค้า - จุฬาลักษณ์</title>
 </head>
-
 <body>
-
-<h1>จัดการสินค้า - จุฬาลักษณ์</h1>
-
-<?php echo "แอดมิน: ". $_SESSION['a_name']; ?> <br>
-
+<h1>หน้าจัดการสินค้า - จุฬาลักษณ์</h1>
+<?php echo "แอดมิน" $_SESSION['a_name'];?><br>
 <ul>
-	<a href="products.php"><li>จัดการสินค้า</li></a>
-	<a href="orders.php"><li>จัดการออเดอร์</li></a>
-	<a href="customers.php"><li>จัดการลูกค้า</li></a>
-	<a href="logout.php"><li>ออกจากระบบ</li></a>
+<a href="products.php"><li>จัดการสินค้า</li></a>
+<a href="orders.php"><li>จัดการออเดอร์</li></a>
+<a href="customers.php"><li>จัดการลูกค้า</li></a>
+<a href="logout.php"><li>ออกจากระบบ</li></a>
 </ul>
 </body>
-</html>
