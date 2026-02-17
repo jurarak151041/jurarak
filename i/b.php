d40a74415a7a7054c9e4f920f7c3ab70a5e2f82f
jurarak151041 <Juralak151041@gmail.com>
2026-02-17 20:05:26 +0700

    a

---
+++ i/b.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>จุฬาลักษณ์ ลมดา (พลอย)</title>
+</head>
+
+<body>
+<h1>งาน i -- จุฬาลักษณ์ ลมดา (พลอย)</h1>
+
+<form method="post" action="" enctype="multipart/form-data">
+	ชื่อจังหวัด <input type="text" name="pname" autofocus required>
+    รูป <input type="file" name="pimag" required><br>
+    <button type="submit" name="Submit">บันทึก</button>
+    <button type="submit" name="Submit">บันทึก</button>	
+</form><br><br>
+
+<?php
+if(isset($_POST['Submit'])){
+	include_once("connectdb.php");
+	$rname = $_POST['rname'];
+	$sql2 = "INSERT INTO regions (r_id, r_name) VALUES (NULL, '{$rname}')";
+	mysqli_query($conn, $sql2) or die ("เพิ่มข้อมูลไม่ได้");
+}
+?>
+
+
+<table border="1">
+	<tr>
+    	<th>รหัสภาค</th>
+        <th>ชื่อภาค</th>
+        <th>ลบ</th>
+    </tr>
+<?php
+include_once("connectdb.php");
+$sql = "SELECT * FROM `provinces` ";
+$rs = mysqli_query($conn, $sql);
+ while ($data = mysqli_fetch_array($rs)){
+?>   
+    <tr>
+    	<td><?php echo $data['r_id'] ; ?></td>
+        <td><?php echo $data['r_name'] ;?></td>
+        <td><img src="images/<?php echo $data['p_id'] ;?>.jpg" width="140"></td>
+        <td width="80" align="center"><a href="delete_region.php?id=<?php echo $data['r_id'] ; ?>" onClick="return confirm('ยืนยันการลบ?')"><img src="images/Delete.jpg" width="20"></td>
+    </tr>
+<?php } ?>
+</table>
+
+</body>
+</html>
+
+<?php
+mysqli_close($conn);
+?>
