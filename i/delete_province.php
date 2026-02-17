0dd619d547d0eefe3ca5769738b1e226750a2ee4
jurarak151041 <Juralak151041@gmail.com>
2026-02-17 21:23:43 +0700

    ด

--- i/delete_province.php
+++ i/delete_province.php
@@ -2,15 +2,16 @@
 <?php
 include_once("connectdb.php");
 
-$id=$_GET['id'];
+$id = $_GET['id'];
 $ext = $_GET['ext'];
-
-$sql="DELETE FROM `provinces` WHERE `p_id`={$id} ";
-mysqli_query($conn,$sql) or die ("ลบข้อมูลไม่ได้");
+$sql = "DELETE FROM provinces WHERE p_id='{$id}' ";
+mysqli_query($conn, $sql) or die ("ลบข้อมูลไม่ได้");
 
 unlink("images/".$id.".".$ext);
 
-echo"<script>";
+
+echo "<script>";
 echo "window.location='b.php';";
-echo"</script>";
+echo "</script>";
+
 ?>
