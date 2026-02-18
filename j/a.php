a30dc57c5ad04a5fc09cbb450c9561bf80cacc60
jurarak151041 <jurarak151041@gmail.com>
2026-02-18 13:50:04 +0700

    a

--- j/a.php
+++ j/a.php
@@ -2,10 +2,13 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>งาน j - 66010914045 จุฬาลักษณ์ ลมดา (พลอย)</title>
+<title>งาน j จุฬาลักษณ์ ลมดา (พลอย)</title>
 </head>
 
 <body>
+<h1>66010914045 จุฬาลักษณ์ ลมดา (พลอย)</h1>
+
+<img src="1.jpeg" width="400">
 
 </body>
 </html>
