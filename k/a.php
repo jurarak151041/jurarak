c8a555f2caf52ad78e6b77f43443c9f777b33e24
jurarak151041 <jurarak151041@gmail.com>
2026-02-18 16:49:02 +0700

    i

---
+++ k/a.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>จุฬาลักษณ์ ลมดา (พลอย)</title>
+</head>
+
+<body>
+<h1>งาน k 66010914045 จุฬาลักษณ์ ลมดา (พลอย)</h1>
+<button type="submit" name="Submit">Click</button>
+<button type="submit" name="Submit">Click!!</button>
+
+
+</body>
+</html>
