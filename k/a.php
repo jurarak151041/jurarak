f0983a0b59f1e6fd4300b0050cbb3c12623a1a7a
jurarak151041 <jurarak151041@gmail.com>
2026-02-18 17:00:31 +0700

    ้้า่

--- k/a.php
+++ k/a.php
@@ -5,11 +5,37 @@
 <title>จุฬาลักษณ์ ลมดา (พลอย)</title>
 </head>
 
-<body>
-<h1>งาน k 66010914045 จุฬาลักษณ์ ลมดา (พลอย)</h1>
-<button type="submit" name="Submit">Click color="#1EFF00"</button>
-<button type="submit" name="Submit">Click!!</button>
+<button class="btn-green" onclick="showImage('images/2.jpg')">Click</button>
+    <button class="btn-orange" onclick="showImage('images/1.jpg')">Click</button>
 
+    <div id="myModal" class="modal">
+        <span class="close" onclick="closeImage()">&times; ปิดรูปภาพ</span>
+        <img class="modal-content" id="imgFull">
+    </div>
+
+    <script>
+        var modal = document.getElementById("myModal");
+        var modalImg = document.getElementById("imgFull");
+
+        // ฟังก์ชันเปิดรูป
+        function showImage(src) {
+            modal.style.display = "block";
+            modalImg.src = src;
+        }
+
+        // ฟังก์ชันปิดรูป (แล้วจะเห็นหน้าหลักเหมือนเดิม)
+        function closeImage() {
+            modal.style.display = "none";
+        }
+
+        // คลิกพื้นที่ว่างข้างนอกรูปเพื่อปิดก็ได้
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                closeImage();
+            }
+        }
+    </script>
 
 </body>
 </html>
+
