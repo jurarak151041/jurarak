78347923a2d7e7ca9adeb92b44a7100ec56db8f0
jurarak151041 <jurarak151041@gmail.com>
2026-02-18 14:17:56 +0700

    s

--- i/a.php
+++ i/a.php
@@ -1,3 +1,21 @@
+<?php
+include_once("connectdb.php");
+
+if(isset($_POST['Submit'])){
+    $pname = $_POST['pname'];
+    $ext = pathinfo($_FILES['pimage']['name'], PATHINFO_EXTENSION);
+    $rid = $_POST['rid'];
+    
+    $sql_insert = "INSERT INTO provinces (p_id, p_name, p_ext, r_id) VALUES (NULL, '{$pname}', '{$ext}', '{$rid}')";
+    mysqli_query($conn, $sql_insert) or die ("เพิ่มข้อมูลไม่ได้: " . mysqli_error($conn));
+    
+    $pid = mysqli_insert_id($conn);
+    
+    move_uploaded_file($_FILES['pimage']['tmp_name'], "images/".$pid.".".$ext);
+}
+?>
+
+
 <!doctype html>
 <html>
 <head>
@@ -8,44 +26,61 @@
 <body>
 <h1>งาน i -- จุฬาลักษณ์ ลมดา (พลอย)</h1>
 
-<form method="post" action="" >
-	ชื่อภาค <input type="text" name="rname" autofocus required>
-    <button type="submit" name="Submit">บันทึก</button>	
-</form><br><br>
+<form method="post" action="" enctype="multipart/form-data" >
+    ชื่อจังหวัด <input type="text" name="pname" autofocus required><br>
+    รูปภาพ <input type="file" name="pimage" required> <br>
+    
+    ภาค
+    <select name="rid">
+    <?php
+    $sql_region = "SELECT * FROM regions";
+    $rs_region = mysqli_query($conn, $sql_region);
+    while ($data_region = mysqli_fetch_array($rs_region)){
+    ?>   
+        <option value="<?php echo $data_region['r_id'] ; ?>"><?php echo $data_region['r_name'] ;?></option>
+    <?php } ?>
+    </select>
+    <br><br>
+    
+    <button type="submit" name="Submit">บันทึก</button> 
+</form>
+<br><hr><br>
 
-<?php
-if(isset($_POST['Submit'])){
-	include_once("connectdb.php");
-	$rname = $_POST['rname'];
-	$sql2 = "INSERT INTO regions (r_id, r_name) VALUES (NULL, '{$rname}')";
-	mysqli_query($conn, $sql2) or die ("เพิ่มข้อมูลไม่ได้");
-}
-?>
-
-
-<table border="1">
-	<tr>
-    	<th>รหัสภาค</th>
+<table border="1" cellpadding="5" cellspacing="0">
+    <tr>
+        <th>รหัสจังหวัด</th>
+        <th>ชื่อจังหวัด</th>
         <th>ชื่อภาค</th>
+        <th>รูป</th>
         <th>ลบ</th>
     </tr>
 <?php
-include_once("connectdb.php");
-$sql = "SELECT * FROM regions";
-$rs = mysqli_query($conn, $sql);
- while ($data = mysqli_fetch_array($rs)){
+$sql_show = "SELECT * FROM provinces AS p INNER JOIN regions AS r ON p.r_id = r.r_id ORDER BY p.p_id ASC";
+$rs_show = mysqli_query($conn, $sql_show);
+
+while ($data = mysqli_fetch_array($rs_show)){
 ?>   
     <tr>
-    	<td><?php echo $data['r_id'] ; ?></td>
+        <td align="center"><?php echo $data['p_id'] ; ?></td>
+        <td><?php echo $data['p_name'] ;?></td>
         <td><?php echo $data['r_name'] ;?></td>
-        <td width="80" align="center"><a href="delete_region.php?id=<?php echo $data['r_id'] ; ?>" onClick="return confirm('ยืนยันการลบ?')"><img src="images/Delete.jpg" width="20"></td>
+        
+        <td align="center">
+            <?php if($data['p_ext'] != "") { ?>
+                <img src="images/<?php echo $data['p_id']; ?>.<?php echo $data['p_ext']; ?>" width="100">
+            <?php } else { echo "ไม่มีรูป"; } ?>
+        </td>
+        
+        <td align="center">
+            <a href="delete_provinces.php?id=<?php echo $data['p_id']; ?>&ext=<?php echo $data['p_ext']; ?>" onClick="return confirm('ยืนยันการลบข้อมูลนี้?');">
+                <img src="images/Delete.jpg" width="30">
+                
+                </a>
+        </td>
     </tr>
 <?php } ?>
 </table>
 
 </body>
 </html>
-
-<?php
-mysqli_close($conn);
-?>
+<?php mysqli_close($conn); ?>
